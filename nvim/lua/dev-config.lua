75ef573f754a6d42915296fb3655417aa661d6dc
Nick Holik <nholik@gmail.com>
2022-01-22 17:58:00 -0500

    add tsserver to dev config

--- nvim/lua/dev-config.lua
+++ nvim/lua/dev-config.lua
@@ -170,6 +170,12 @@ local function setup_dev()
         }
     }
 
+    lspconfig.tsserver.setup {
+        cmd = {"typescript-language-server", "--stdio"},
+        filetypes = {"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"},
+        init_options = {hostInfo = "neovim"}
+    }
+
     lspconfig.efm.setup {
         init_options = {documentFormatting = true},
         filetypes = {"lua"},
