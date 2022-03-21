cbc29136718c39ada50668e15ac394b9b5584968
Nick Holik <nholik@gmail.com>
2022-03-20 20:44:27 -0400

    add trouble window

--- nvim/lua/dev-config.lua
+++ nvim/lua/dev-config.lua
@@ -13,6 +13,53 @@ end
 local function setup_dev()
     require('nvim_comment').setup()
     require('nvim-autopairs').setup()
+    require('trouble').setup({
+        position = "bottom", -- position of the list can be: bottom, top, left, right
+        height = 10, -- height of the trouble list when position is top or bottom
+        width = 50, -- width of the list when position is left or right
+        icons = true, -- use devicons for filenames
+        mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+        fold_open = "", -- icon used for open folds
+        fold_closed = "", -- icon used for closed folds
+        group = true, -- group results by file
+        padding = true, -- add an extra new line on top of the list
+        action_keys = { -- key mappings for actions in the trouble list
+            -- map to {} to remove a mapping, for example:
+            -- close = {},
+            close = "q", -- close the list
+            cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+            refresh = "r", -- manually refresh
+            jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+            open_split = {"<c-x>"}, -- open buffer in new split
+            open_vsplit = {"<c-v>"}, -- open buffer in new vsplit
+            open_tab = {"<c-t>"}, -- open buffer in new tab
+            jump_close = {"o"}, -- jump to the diagnostic and close the list
+            toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+            toggle_preview = "P", -- toggle auto_preview
+            hover = "K", -- opens a small popup with the full multiline message
+            preview = "p", -- preview the diagnostic location
+            close_folds = {"zM", "zm"}, -- close all folds
+            open_folds = {"zR", "zr"}, -- open all folds
+            toggle_fold = {"zA", "za"}, -- toggle fold of current file
+            previous = "k", -- preview item
+            next = "j" -- next item
+        },
+        indent_lines = true, -- add an indent guide below the fold icons
+        auto_open = false, -- automatically open the list when you have diagnostics
+        auto_close = false, -- automatically close the list when you have no diagnostics
+        auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+        auto_fold = false, -- automatically fold a file trouble list at creation
+        auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+        signs = {
+            -- icons / text used for a diagnostic
+            error = "",
+            warning = "",
+            hint = "",
+            information = "",
+            other = "﫠"
+        },
+        use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+    })
     require('nvim-treesitter.configs').setup({
         -- One of "all", "maintained" (parsers with maintainers), or a list of languages
         ensure_installed = "maintained",
@@ -196,7 +243,12 @@ local function setup_dev()
         buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
         buf_set_keymap("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
         buf_set_keymap("n", "<leader>fd", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
+        buf_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
+        buf_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
+        buf_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
+        buf_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
+        buf_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
+        buf_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", opts)
         if (client.name == 'tsserver') then
             buf_set_keymap("n", "<leader>gs", "<cmd>TSLspOrganize<CR>", opts)
             buf_set_keymap("n", "<leader>gr", "<cmd>TSLspRenameFile<CR>", opts)
